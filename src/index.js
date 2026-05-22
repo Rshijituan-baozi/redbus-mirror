91b77b394df3ab1c734d08b07c2e2254182f7231
Rshijituan <rshijituan@gmail.com>
2026-05-23 04:54:29 +0800

    revert: remove express.json()

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ process.on('unhandledRejection', (reason) => { console.error('[REJECT]', reason)
 
 const PORT = parseInt(process.env.PORT || '3000');
 const app = express();
-app.use(express.json());
 
 const mainProxy = createRedbusProxy(process.env.PUBLIC_HOST || `localhost:${PORT}`);
 
